aeda13a5e16b7d7be28baeeb910edc57490689ae
Alvaro D. <alvarodeguer@gmail.com>
2024-04-04 13:53:33 -0300

    Add Exercise 1

---
+++ Homework 1 - String Arithmetic Operations/Exercise-1.js
@@ -0,0 +1,14 @@
+function stringPlus(string1) {
+    
+    let string2 = '44';
+
+    const num1 = parseInt(string1);
+    const num2 = parseInt(string2);
+
+    const sum = num1 + num2;
+
+    return sum;
+}
+
+const result = stringPlus('100');
+console.log(result);
