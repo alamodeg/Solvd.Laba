e013d7108c4bb5a80cf32b89e83871145b4d0e24
Alvaro D. <alvarodeguer@gmail.com>
2024-04-04 15:35:32 -0300

    Add Exercise 3

---
+++ Homework 1 - String Arithmetic Operations/Exercise-3.js
@@ -0,0 +1,14 @@
+function stringMultiply(string1) {
+    
+    let string2 = '44';
+
+    const num1 = parseInt(string1);
+    const num2 = parseInt(string2);
+
+    const multiply = num1 * num2;
+
+    return multiply;
+}
+
+const result = stringMultiply('100');
+console.log(result);
