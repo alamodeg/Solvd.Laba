754ea8a77fb352419fe7ee019bb3a4b7fd9d0a4a
Alvaro D. <alvarodeguer@gmail.com>
2024-04-04 22:33:31 -0300

    Correct exercise 1

--- Homework 1 - String Arithmetic Operations/Exercise-1.js
+++ Homework 1 - String Arithmetic Operations/Exercise-1.js
@@ -1,14 +1,39 @@
-function stringPlus(string1) {
+function stringPlus(_string) {
     
-    let string2 = '44';
+    // convert both strings to digits
+    let string1 = this.split('').map(Number);
+    let string2 = _string.split('').map(Number);
 
-    const num1 = parseInt(string1);
-    const num2 = parseInt(string2);
+    // make both array to the same size to sum them
+    while (string1.length < string2.length) {
+        string1.unshift(0);
+    }
+    while (string2.length < string1.length) {
+        string2.unshift(0);
+    }
 
-    const sum = num1 + num2;
+    // make the sum digit by digit taking in count the carry
+    let result = [];
+    let carry = 0;
+    
+    for (let i = string1.length - 1; i >= 0; i--) {
+        let sum = string1[i] + string2[i] + carry;
+        if (sum > 9) {
+            carry = 1;
+            sum -= 10;
+        } else {
+            carry = 0;
+        }
+        result.unshift(sum);
+    }
 
-    return sum;
+    // convert de array of digits into a string
+    return result.join('');
 }
 
-const result = stringPlus('100');
-console.log(result);
+// use a prototype to extend the string function and utilize the previously defined function
+String.prototype.plus = stringPlus;
+
+let string1 = "55555555555555555555555555555555555555555555555555555";
+let string2 = "4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444";
+console.log(string1.plus(string2));
